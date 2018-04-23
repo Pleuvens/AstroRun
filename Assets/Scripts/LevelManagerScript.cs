8baafc7c1d9a5d23914c292102d320c2199a6907
Pleuvens Fervil <pleuvens.fervil@gmail.com>
2018-04-23 17:23:12 +0200

    [GAME] - prototype correct

---
+++ Assets/Scripts/LevelManagerScript.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManagerScript : MonoBehaviour {
+
+    public GameObject[] roadBlocs;
+    public GameObject[] obstacles;
+    public List<GameObject> roadList;
+    public List<GameObject> obstaclesList;
+    public GameObject Player;
+    public Canvas GUI;
+    public float zPos = 0f;
+    public float ozPos = 0f;
+    int count = 0;
+
+	// Use this for initialization
+	void Start () {
+        GUI.transform.GetChild(1).gameObject.SetActive(false);
+        GUI.transform.GetChild(2).gameObject.SetActive(false);
+        for (int i = 0; i < 18; ++i)
+        {
+            Addbloc(roadBlocs[0], ref zPos);
+        }
+        for (int i = 0; i < 7; i++)
+        {
+            AddObstacles(obstacles[0], ref ozPos);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player.GetComponent<PlayerScript>().isGameOver)
+        {
+            GUI.transform.GetChild(1).gameObject.SetActive(true);
+            GUI.transform.GetChild(2).gameObject.SetActive(true);
+        }
+        GameObject rtmp = roadList[roadList.Count - 1];
+        GameObject otmp = obstaclesList[obstaclesList.Count - 1];
+        if (rtmp.transform.position.z - Player.transform.position.z < -8)
+        {
+            Movebloc(ref zPos);
+        }
+        if (otmp.transform.position.z - Player.transform.position.z < -10)
+        {
+            MoveObstacles(ref ozPos);
+        }
+    }
+
+    void Addbloc(GameObject blocModel, ref float pos)
+    {
+        GameObject newBloc = (GameObject)Instantiate(blocModel, new Vector3(0, 0, zPos), Quaternion.identity);
+        pos += 5;
+        newBloc.transform.SetParent(this.transform);
+        roadList.Insert(0, newBloc);
+    }
+
+    void Movebloc(ref float pos)
+    {
+        GameObject newBloc = roadList[roadList.Count - 1];
+        roadList.Remove(newBloc);
+        newBloc.transform.position = new Vector3(newBloc.transform.position.x, newBloc.transform.position.y, pos);
+        pos += 5;
+        newBloc.SetActive(true);
+        roadList.Insert(0, newBloc);
+    }
+
+    void AddObstacles(GameObject obstacle, ref float zpos)
+    {
+        float xpos = Random.Range(-4f, 5f);
+        float ypos = Random.Range(1f, 5f);
+        zpos += 10 + Random.Range(0, 20);
+        Vector3 pos = new Vector3(xpos, ypos, zpos);
+        GameObject obs = (GameObject)Instantiate(obstacle, pos, Quaternion.identity);
+        obs.transform.SetParent(this.transform);
+        obstaclesList.Insert(0, obs);
+    }
+
+    void MoveObstacles(ref float pos)
+    {
+        int destroyRoad = Random.Range(0, 10);
+        if (Player.transform.position.z > 50 && destroyRoad == 6)
+        {
+            GameObject tmp = roadList[0];
+            GameObject tmp2 = roadList[1];
+            tmp.SetActive(false);
+            tmp2.SetActive(false);
+            pos += Player.GetComponent<PlayerScript>().jumpDistance + Random.Range(0, 20);
+            return;
+        }
+        GameObject obs = obstaclesList[obstaclesList.Count - 1];
+        obstaclesList.Remove(obs);
+        obs.transform.position = new Vector3(Random.Range(-4f, 5f), Random.Range(1f, 5f), pos);
+        pos += Player.GetComponent<PlayerScript>().jumpDistance + Random.Range(0, 20);
+        obstaclesList.Insert(0, obs);
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour {
+
+    public GameObject smoke;
+
+    int maxSpeed = 60;
+    public float speed = 18.0F;
+    //public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    public long d = 0;
+    public bool isGameOver = false;
+    private Vector3 moveDirection = Vector3.zero;
+    float jumpHeight = 4f;
+    public float jumpDistance = 0f;
+    public float cooldown = 0f;
+    public float yMove = 0f;
+
+    public Vector3 destination = Vector3.zero;
+
+    public Text dist;
+
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("hasStarted", true);
+    }
+
+	void Update () {
+        if (isGameOver)
+            return;
+        if (cooldown > 0)
+            cooldown -= Time.deltaTime;
+        dist.text = this.transform.position.z.ToString("F0");
+        jumpDistance = speed+ 1 / 2 * gravity;
+        smoke.SetActive(true);
+        CharacterController controller = GetComponent<CharacterController>();
+        if (speed < maxSpeed && this.transform.position.z > d)
+        {
+            speed += 1;
+            d += 100;
+        }
+        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 1);
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+        if (controller.isGrounded)
+        {
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = Mathf.Sqrt(2 * gravity * jumpHeight);
+                moveDirection.x = 0f;
+            }           
+        }
+        else
+        {
+            smoke.SetActive(false);
+            moveDirection.y = yMove;
+        }
+
+        int xMove = Mathf.RoundToInt(moveDirection.x);
+        if (xMove > 0)
+            xMove = 4;
+        else if (xMove < 0)
+            xMove = -4;
+        
+        if (cooldown <= 0)
+        {
+            destination = Vector3.zero;
+            cooldown = 0;
+            if (xMove != 0)
+                cooldown += 0.2f;
+            if (xMove < 0 && this.transform.position.x > -2)
+                destination = new Vector3((xMove + this.transform.position.x), 0, this.transform.position.z);
+            else if (xMove > 0 && this.transform.position.x < 2)
+                destination = new Vector3((xMove + this.transform.position.x), 0, this.transform.position.z);
+        }
+        else
+        {
+            if (destination != Vector3.zero && Mathf.Abs(this.transform.position.x - destination.x) > 0.1)
+            {
+                Vector3 inter = this.transform.position; 
+                inter.x = (destination.x + inter.x) / 2;
+                this.transform.position = inter;
+            }
+        }
+        moveDirection.x = 0;
+        moveDirection.y -= gravity * Time.deltaTime;
+        yMove = moveDirection.y;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.collider.gameObject.tag == "Obstacle")
+        {
+            isGameOver = true;
+            anim.SetBool("hasStarted", false);
+            smoke.SetActive(false);
+        }
+    }
+}
